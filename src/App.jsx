82157b9a1b0e198afa02692519ac0aa483d5bfa1
manudev-1 <manuelebarone186@gmail.com>
2023-05-03 20:08:08 +0200

    Phone Query

--- src/App.jsx
+++ src/App.jsx
@@ -61,43 +61,45 @@ export function App() {
 
   return (
     <div className={`App ${datas.length > 0 ? 'h-fit' : 'h-screen'} w-full flex justify-center bg-backG`}>
-      <div className="fixed w-full pointer-events-none">
+      <div className="fixed w-full h-screen pointer-events-none flex flex-col justify-between xl:h-fit xl:block">
         <section className="flex justify-between items-start">
-            <img src={sxPlant} alt="sx Plant" className="w-1/5" />
-            <img src={dxPlant} alt="dx Plant" className="w-1/5" />
+            <img src={sxPlant} alt="sx Plant" className="w-1/3 xl:w-1/5" />
+            <img src={dxPlant} alt="dx Plant" className="w-1/3 xl:w-1/5" />
         </section>
         <section className="flex justify-between items-baseline">
-            <img src={sxMenso} alt="sx Men" className="w-1/5" />
-            <img src={dxMenso} alt="dx Men" className="w-1/5" />
+            <img src={sxMenso} alt="sx Men" className="w-1/3 xl:w-1/5" />
+            <img src={dxMenso} alt="dx Men" className="w-1/3 xl:w-1/5" />
         </section>
       </div>
-      <section className="mt-16 flex flex-col items-center max-w-lg text-center text-text font-calibri text-lg">
-        <h1 className="font-cocoGoose text-8xl text-title">BOTANY</h1>
+      <section className="mt-24 flex flex-col items-center max-w-xs text-center text-text font-calibri text-base xl:text-lg xl:mt-16 xl:max-w-lg">
+        <h1 className="font-cocoGoose text-2xl text-title xl:text-8xl">BOTANY</h1>
         <p>Look for the plant that has attracted your attention and discover its details or find out how to cure yours.</p>
         <section className="mt-16 max-w-lg flex">
-            <input type="text" name="plant" id="query" className="w-96 h-10 rounded-full p-5 outline-none capitalize" placeholder="Write the name of a plant..." maxLength="25" onInput={handleChange} value={query}/>
+            <input type="text" name="plant" id="query" className="w-72 h-10 rounded-full p-5 outline-none capitalize xl:w-96" placeholder="Write the name of a plant..." maxLength="25" onInput={handleChange} value={query}/>
             <img src={searchI} alt="ricerca" className="w-5 relative -translate-x-10 cursor-pointer rounded-full" id="search" onClick={handleReq}/>
         </section>
-        <div className="block">
+        <div className="">
           {
             datas.map((data) => {
               return(
-                <section key={data.id} className="mt-10 bg-white w-96 h-96 rounded-xl overflow-hidden text-left hover:shadow-2xl duration-500">
+                <section key={data.id} className="mt-10 bg-white w-60 h-60 rounded-xl overflow-hidden text-left hover:shadow-2xl duration-500 xl:w-96 xl:h-96">
                   <img src={data.default_image.original_url === 'https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/og/49255769768_df55596553_b.jpg' ? defaultNotProvided : data.default_image.original_url} alt={`${data.id} Plant`} className="h-4/6 w-full object-cover"/>
                   <div className="p-2 h-2/6 flex flex-col justify-between items-start">
                     <section className='w-full'>
                       <h1>{data.common_name}</h1>
                       <p className="text-gray-500 overflow-hidden text-ellipsis w-[calc(100%-3rem)] whitespace-nowrap" id="desc">{data.scientific_name}</p>
                     </section>
-                    <button className='bg-backG py-2 px-5 rounded-lg shadow-xl' onClick={() => {handleSingle(data.id)}}>More Info</button>
+                    <div className="w-full flex justify-end xl:block">
+                      <button className='bg-backG w-5 h-5 flex justify-center font-bold items-center rounded-lg shadow-xl xl:w-fit xl:h-fit xl:font-normal xl:py-2 xl:px-5' onClick={() => {handleSingle(data.id)}}>{window.innerWidth < 1280 ? '+' : 'More Info'}</button>
+                    </div>
                     <div className={`fixed inset-0 flex justify-center items-center bg-backSmoked duration-500 ${data.single ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-                      <section className='w-1/2 h-1/2 bg-white rounded-2xl overflow-hidden'>
+                      <section className='w-11/12 h-4/6 bg-white rounded-2xl overflow-hidden xl:w-1/2 xl:h-1/2'>
                         <div className="relative m-3 float-right cursor-pointer" onClick={() => {handleSingle(data.id)}}>
                           <div className="h-1 w-5 rounded-xl bg-title m-1 rotate-45 translate-y-1"></div>
                           <div className="h-1 w-5 rounded-xl bg-title m-1 -rotate-45 -translate-y-1"></div>
                         </div>
                         <div className="w-full h-1/3 pl-5 pt-5 bg-backG">
-                          <img src={data.default_image.small_url === 'https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/small/49255769768_df55596553_b.jpg' ? defaultNotProvided : data.default_image.small_url} alt={data.default_image.small_url} className='w-1/6 h-[calc(100% + 1rem)] object-cover rounded-lg'/>
+                          <img src={data.default_image.small_url === 'https://perenual.com/storage/species_image/2_abies_alba_pyramidalis/small/49255769768_df55596553_b.jpg' ? defaultNotProvided : data.default_image.small_url} alt={data.default_image.small_url} className='w-1/3 h-[calc(100%+20px)] object-cover rounded-lg xl:w-1/6 xl:h-[calc(100% + 1rem)]'/>
                         </div>
                         <div className="flex justify-between h-2/3">
                           <section className='pt-6 pl-5 font-bold flex flex-col justify-evenly'>
@@ -115,7 +117,7 @@ export function App() {
                               }</div></h1>
                             <h1>Watering: <span className='text-gray-500 font-normal'>{data.watering}</span></h1>
                           </section>
-                          <img src={dxMenso} alt="" className='w-56'/>
+                          {window.innerWidth < 1280 ? '' : <img src={dxMenso} alt="" className='w-56'/>}
                         </div>
                       </section>
                     </div>
